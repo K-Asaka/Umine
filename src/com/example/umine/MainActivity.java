a9f253ea07c64811de53e5b8d4d5f965153f0105
Raimu <Raimu@Raimu-PC.mshome.net>
2015-06-13 18:55:38 +0900

    tes

--- src/com/example/umine/MainActivity.java
+++ src/com/example/umine/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.umine;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	private Camera cam;
+	final private String saveDir=Environment.getExternalStorageDirectory().getPath()+"/test999/";
+	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
 
+		try{
+		cam=Camera.open();
+		}catch(Exception e){
+			this.finish();
+		}
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        LinearLayout l = new LinearLayout(this);
+        l.addView(new CamView(this,cam));
+        setContentView(l);
+//
+//		Camera cam;
+//		try {
+//			cam=Camera.open();
+//		} catch (Exception e) {
+//			finish();
+//		}
+//
+//
+//
+//
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -31,4 +56,15 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+/*
+	@Override
+	public void onPause(){
+		super.onPause();
+		if(cam!=null){
+			cam.release();
+			cam=null;
+
+		}
+	}
+	*/
 }
