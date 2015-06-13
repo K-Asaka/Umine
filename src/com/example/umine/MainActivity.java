f2fd69d0d9da0c4422195a81c99e486f1289b9bd
kotaro3 <skouta310@gmail.com>
2015-06-13 16:20:04 +0900

    GITめんどくさ

--- src/com/example/umine/MainActivity.java
+++ src/com/example/umine/MainActivity.java
@@ -2,33 +2,43 @@ package com.example.umine;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	@Override
+	static int cnt ;
+	String cntText = "";
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button countBtn = (Button)findViewById(R.id.countBtn);
+		Button tweetBtn = (Button)findViewById(R.id.tweet);
+		final TextView counter = (TextView)findViewById(R.id.counter);
+
+
+		countBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				cnt += 1;
+				cntText = String.valueOf(cnt);
+				counter.setText(cntText);
+			}
+		});
+
+		tweetBtn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+
+			}
+		});
+
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
